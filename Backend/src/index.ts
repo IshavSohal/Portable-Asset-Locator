37c97d52c48142370dd1f48ec9d5577aa6b353ae
Quincy Garcia <Quincy.Garcia@ec.gc.ca>
2024-06-17 12:43:10 -0400

    Setup Express server (no tests)

---
+++ Backend/src/index.ts
@@ -0,0 +1,20 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+app.get("/start", (req: Request, res: Response) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
