66767e06a1d60e0ddfacc9599335b2ace158532b
Quincy Garcia <Quincy.Garcia@ec.gc.ca>
2024-06-18 12:35:53 -0400

    Refactor, routing, and controller

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,19 +1,14 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { routes } from "./Routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
-
-app.get("/start", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
