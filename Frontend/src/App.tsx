6e3625583b2cbf15ec451d54f9a6bda36ffedc80
Benjamin Lee <Benjamin.Lee@ec.gc.ca>
2024-07-03 14:01:27 -0400

    Changed App.tsx and added components page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
