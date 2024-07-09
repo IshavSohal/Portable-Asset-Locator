a5343b65e839347fb49a7755fcd11f621cf0c586
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-09 17:08:53 -0400

    Moved top nav into the header as per GC design system docs

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,42 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { GcdsHeader, GcdsTopNav, GcdsNavLink } from '@cdssnc/gcds-components-react';
+import {
+    GcdsHeader,
+    GcdsTopNav,
+    GcdsNavLink,
+} from '@cdssnc/gcds-components-react';
 
 function App() {
-  return (
-    <div className="App">
-      <GcdsHeader
-      langHref="#"
-      skipToHref="#"
-      >
-      </GcdsHeader>
+    return (
+        <div className='App'>
+            <GcdsHeader langHref='#' skipToHref='#'>
+                <GcdsTopNav
+                    slot='menu'
+                    label='Top navigation'
+                    alignment='right'
+                >
+                    <GcdsNavLink href='#home' slot='home'>
+                        Portable Asset Locator
+                    </GcdsNavLink>
+                    <GcdsNavLink href='#'>Why GC Notify</GcdsNavLink>
+                    <GcdsNavLink href='#'>Contact us</GcdsNavLink>
+                </GcdsTopNav>
+            </GcdsHeader>
 
-      <GcdsTopNav
-      label="Top navigation"
-      alignment="right"
-      >
-    <GcdsNavLink href="#home" slot="home">Portable Asset Locator</GcdsNavLink> 
-    <GcdsNavLink href="#">Why GC Notify</GcdsNavLink>
-    <GcdsNavLink href="#">Contact us</GcdsNavLink>
-    </GcdsTopNav>
-    
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
