4ba53ae44cb801b0e475119253cb7c47d54330c5
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-10 14:06:05 -0400

    add basic link from dashboard page to home

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <div>
+                <Link to='/'>Home</Link>
+            </div>
         </div>
     );
 }
