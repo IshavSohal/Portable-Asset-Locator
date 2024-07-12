a2fbeaa8a38f6a854129a8baa2824ff02fef42f7
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-12 15:20:42 -0400

    refactor: move api request to separate file

---
+++ Frontend/src/requests/simple.ts
@@ -0,0 +1,20 @@
+const fetchHelloWorld: () => Promise<string> = async () => {
+    // During development, we want to run the client
+    // with the faster npm run start 
+    // (instead of creating a build everytime a change is made)
+    // let server = 'http://localhost:8080';
+    // if (process.env.NODE_ENV !== 'production') {
+    //     server = 'http://localhost:8080' // TODO: Resolve CORS
+    // }
+
+    const res = await fetch('/api/start');
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        const errorText = await res.text();
+
+        return 'Error fetching data: ' + errorText;
+    }
+    return res.text();
+}
+
+export { fetchHelloWorld };
