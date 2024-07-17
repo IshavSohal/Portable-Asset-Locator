d9cc607abdeb4429d60bd4130a0afd6a004f0a71
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-17 14:32:40 -0400

    Added example UI error handling and loading with requests

--- Frontend/src/components/MessageDisplay.tsx
+++ Frontend/src/components/MessageDisplay.tsx
@@ -3,19 +3,34 @@ import { fetchHelloWorld } from '../requests/simple';
 
 function MessageDisplay() {
     const [message, setMessage] = useState('');
+    const [error, setError] = useState<null | string>(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch data
         const dataFetch = async () => {
-            const message = await fetchHelloWorld();
-            // set state when the data received
-            setMessage(message);
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            try {
+                const message = await fetchHelloWorld();
+                // set state when the data received
+                setMessage(message);
+            } catch (err) {
+                setError((err as Error).message);
+            } finally {
+                setLoading(false);
+            }
         };
 
         dataFetch();
     }, []);
 
-    return <p>The message from the server is: {message}</p>;
+    if (loading) {
+        return <p>Loading...</p>;
+    } else if (error != null) {
+        return <p>There was an error getting the message... {error}</p>;
+    } else {
+        return <p>The message from the server is: {message}</p>;
+    }
 }
 
 export default MessageDisplay;
