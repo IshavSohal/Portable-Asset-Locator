fcec002009dabb12dfcfbc0b1457b313c2e44b2e
Gordon Lin <Gordon.Lin@ec.gc.ca>
2024-07-23 17:26:47 -0400

    Add tests for registration endpoint

--- Backend/__tests__/Routes/index.test.ts
+++ Backend/__tests__/Routes/index.test.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import app from "../../src/index";
 
 describe("Server setup tests", () => {
-    it("Root test", async () => {
-      const res = await request(app).get("/api")
-      expect(res.statusCode).toEqual(302);
-    });
+  it("Root test", async () => {
+    const res = await request(app).get("/api");
+    expect(res.statusCode).toEqual(302);
   });
+});
