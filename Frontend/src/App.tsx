a67c573135cfcc7648062cb7e9d4e860d5884443
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-24 15:56:30 -0400

    add prettier config

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,45 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
-import Dashboard from "./pages/Dashboard";
-import Registration from "./pages/Registration";
-import MainTemplate from "./templates/MainTemplate";
-import SignOn from "./pages/SignOn";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import Dashboard from './pages/Dashboard'
+import Registration from './pages/Registration'
+import MainTemplate from './templates/MainTemplate'
+import SignOn from './pages/SignOn'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signin" element={<SignOn />} />
-        <Route path="/register" element={<Registration />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/signin" element={<SignOn />} />
+                <Route path="/register" element={<Registration />} />
+            </Routes>
+        </Router>
+    )
 }
 
 function Home() {
-  return (
-    <div className="App">
-      <MainTemplate addMargins={false}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </MainTemplate>
-    </div>
-  );
+    return (
+        <MainTemplate addMargins={false}>
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </div>
+        </MainTemplate>
+    )
 }
 
-export default App;
+export default App
