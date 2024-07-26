30051b887829d354bed2a32d59db94c9f74843e5
IshavSohal <ishav.sohal@mail.utoronto.ca>
2024-07-26 16:01:25 -0400

    fix width of registration inputs

--- Frontend/src/templates/MainTemplate.tsx
+++ Frontend/src/templates/MainTemplate.tsx
@@ -145,12 +145,12 @@ function MainTemplate({
         </GcdsTopNav>
       </GcdsHeader>
 
-      <GcdsContainer size="xl" centered padding="0" margin="0" style={{marginLeft: 10, marginRight: 10}}>
+      <GcdsContainer className="main-content" size="xl" centered padding="0" margin="0" style={{marginLeft: 10}}>
         <div> {children} </div>
       </GcdsContainer>
 
-      <GcdsContainer size="xl" centered style={{backgroundColor: '#33465C', paddingTop: 1, marginBottom: -15 }}>
-          <GcdsText margin-bottom="450" margin-top="450" style={{ paddingBottom: 5, marginLeft: 10}}>
+      <GcdsContainer className="footer-content" size="xl" centered style={{backgroundColor: '#33465C', paddingTop: 1, marginBottom: -15 }}>
+          <GcdsText margin-bottom="450" margin-top="450" style={{ paddingBottom: 5}}>
               <div style={{ color: 'white', fontSize: 24 }}> Portable Asset Locator </div>
           </GcdsText>
       </GcdsContainer>
