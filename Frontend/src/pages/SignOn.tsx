6bff7026cd48a351ea6e47032717678c449f8576
Jenny Zhang <jenny.zhang@canada.ca>
2024-07-29 16:51:09 -0400

    add logIn call, loading ui on SignOn page

--- Frontend/src/pages/SignOn.tsx
+++ Frontend/src/pages/SignOn.tsx
@@ -6,14 +6,40 @@ import {
     GcdsInput,
 } from '@cdssnc/gcds-components-react'
 import MainTemplate from '../templates/MainTemplate'
+import { GcdsButtonCustomEvent } from '@cdssnc/gcds-components/dist/types/components'
+import { useState } from 'react'
+import { useAuth } from '../hooks/AuthProvider'
 
 function SignOn() {
+    const { logIn } = useAuth()
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    async function handleSubmission(
+        e: GcdsButtonCustomEvent<void>
+    ): Promise<void> {
+        // TODO: capture credentials from form
+        const email = 'jenny.zhang@ec.gc.ca'
+        const password = 'aPassword123'
+
+        setIsSubmitting(true)
+        try {
+            await logIn(email, password)
+        } catch (err) {
+            // setError((err as Error).message);
+            alert((err as Error).message)
+        } finally {
+            setIsSubmitting(false)
+        }
+        setIsSubmitting(false)
+    }
+
     return (
         <MainTemplate currentPage="signin">
             <GcdsHeading tag="h1" style={{ marginBottom: 48 }}>
                 {' '}
                 Sign-In{' '}
             </GcdsHeading>
+            {/* TODO: Add Error component */}
             <GcdsInput
                 inputId="input-email"
                 label="Email"
@@ -31,7 +57,12 @@ function SignOn() {
                 style={{ marginBottom: 48 }}
             ></GcdsInput>
 
-            <GcdsButton> Sign in </GcdsButton>
+            <GcdsButton
+                onGcdsClick={(e: any) => handleSubmission(e)}
+                disabled={isSubmitting}
+            >
+                Sign in
+            </GcdsButton>
 
             <GcdsContainer size="xl" centered style={{ paddingBottom: 10 }}>
                 <GcdsDateModified>2024-07-19</GcdsDateModified>
