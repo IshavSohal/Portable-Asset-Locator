13311a3754690f75d4bc61d4882dd098a7917142
Jenny Zhang <jenny.zhang@canada.ca>
2024-08-01 13:57:12 -0400

    update generic get & post fetch wrappers

--- Frontend/src/requests/auth.ts
+++ Frontend/src/requests/auth.ts
@@ -1,23 +1,6 @@
 import { user } from '../types/user'
-interface FetchPost<T> {
-  (endpoint: string, data: any): Promise<any>;
-}
+import { fetchPost, fetchGet } from './requests';
 
-const fetchPost = async(endpoint: string, data: any): Promise<Response> => {
-  const server = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
-
-  const response = await fetch(server + endpoint, {
-    method: "POST",
-    credentials: 'include',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  return response
-
-}
 
 const loginUser = async (data : {email: string, password: string}): Promise<user> => {
   const response = await fetchPost("/api/auth/login", data);
@@ -31,18 +14,27 @@ const loginUser = async (data : {email: string, password: string}): Promise<user
   }
 }
 
-
-const registerUser = async (data : {firstName: string, lastName: string, email: string, password: string}) => {
-
-    const response = await fetchPost("/api/auth/register", data)
-    // const res = await response.json();
-
-    if (response.ok) {
-        return "Created";
-    } else {
-        // propagate Error so that it may be handled on frontend
-        throw new Error(await response.text()); 
-    }
+const loadUser = async() => {
+  const response = await fetchGet("/api/auth/session");
+  if (response.ok) {
+    return await response.json() as user;
+  } else {
+    // propagate Error so that it may be handled on frontend
+    const { status } = response
+    throw new Error("loadUser Error:" + status + await response.text()); 
+  }
 }
 
-export { registerUser, loginUser };
+
+const registerUser = async (data : {firstName: string, lastName: string, email: string, password: string}) => {
+  const response = await fetchPost("/api/auth/register", data)
+  // const res = await response.json();
+  if (response.ok) {
+      return "Created";
+  } else {
+      // propagate Error so that it may be handled on frontend
+      throw new Error(await response.text()); 
+  }
+}
+
+export { registerUser, loginUser, loadUser };
--- Frontend/src/requests/simple.ts
+++ Frontend/src/requests/simple.ts
@@ -1,27 +1,8 @@
-interface FetchCall<T> {
-    (endpoint: string): Promise<T>;
-}
-
-// Experimental - refactoring for more streamlined GET requests.
-// TO-DO: more testing
-const fetchGet: FetchCall<any> = async (endpoint) => {
-    // During development, we want to run the client
-    // with the faster npm run start (instead of npm run build everytime a change is made)
-    const server = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
-    
-    const res = await fetch(server + endpoint, {
-        credentials: 'include'
-    });
-    if (!res.ok) {
-        const errorText = await res.text();
-        throw new Error('FetchGet Error - Error fetching data: ' + errorText);
-    }
-    return res.text();
-}
+import { fetchGet } from "./requests";
 
 
 const fetchHelloWorld: () => Promise<string> = async () => {
-    const message = await fetchGet('/api/HelloWorld/start');
+    const message = await(await fetchGet('/api/HelloWorld/start')).text();
     return message as string;
 }
 
