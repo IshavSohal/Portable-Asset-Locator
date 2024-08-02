01f0a9296d4a481abbfbf849fafc3d7da92d8e5a
Jenny Zhang <jenny.zhang@canada.ca>
2024-08-02 17:46:56 -0400

    add basic table on dashboard

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,20 +1,83 @@
-import { Link } from "react-router-dom";
-import MainTemplate from "../templates/MainTemplate";
-import { GcdsHeading } from "@cdssnc/gcds-components-react";
+import { Link } from 'react-router-dom';
+import MainTemplate from '../templates/MainTemplate';
+import { GcdsHeading, GcdsPagination } from '@cdssnc/gcds-components-react';
 import MessageDisplay from '../components/MessageDisplay';
 
 function Dashboard() {
-  return (
-    <MainTemplate addMargins={false}>
-      <GcdsHeading tag="h1">Dashboard</GcdsHeading>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
+    const userAssets: asset[] = exampleAssets;
+
+    return (
+        <MainTemplate addMargins={false}>
+            <GcdsHeading tag="h1">Dashboard</GcdsHeading>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
             <div>
                 <MessageDisplay />
             </div>
-    </MainTemplate>
-  );
+            <h2>Your assets</h2>
+            <h3>Assigned to you</h3>
+            <p>Here are all the assets that are currently assigned to you.</p>
+            <p>Showing {userAssets.length} results.</p>
+            <div>
+                <table>
+                    <caption>Table of your current assigned assets</caption>
+                    <thead>
+                        <tr>
+                            <th>Asset</th>
+                            <th>Asset tag</th>
+                            <th>Type</th>
+                            <th>Assignment start date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userAssets.map((asset, index) => {
+                            return (
+                                <tr key={asset.id}>
+                                    <td>{asset.name}</td>
+                                    <td>{asset.tag}</td>
+                                    <td>{asset.type}</td>
+                                    <td>{asset.startDate.toDateString()}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+
+                    <tr></tr>
+                </table>
+                {/* <GcdsPagination
+                label="Pagination"
+                totalPages={2}
+                currentPage={1}
+            ></GcdsPagination> */}
+            </div>
+        </MainTemplate>
+    );
 }
 
 export default Dashboard;
+
+type asset = {
+    id: number;
+    name: string;
+    tag: string;
+    type: string;
+    startDate: Date;
+};
+
+const exampleAssets = [
+    {
+        id: 1,
+        name: 'Dell Laptop',
+        tag: 'A12345',
+        type: 'Laptop',
+        startDate: new Date('August 1, 2024'),
+    },
+    {
+        id: 2,
+        name: 'Dell Mouse',
+        tag: 'A12367',
+        type: 'Mouse',
+        startDate: new Date('August 1, 2024'),
+    },
+];
