26e729fda271b146f73b96ea70e98a58e77f67cb
Jenny Zhang <jenny.zhang@canada.ca>
2024-08-20 16:38:50 -0400

    Check requesting custodian and auto-set start and end dates for assignment

--- Backend/src/Routes/AssignmentRoutes.ts
+++ Backend/src/Routes/AssignmentRoutes.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response, Router } from "express";
 import { AssignmentController } from "../Controllers/AssignmentController";
 import { ConsoleLogger } from "../Logging/ConsoleLogger";
 import { body } from "express-validator";
-const { validationResult } = require('express-validator');
+import { AssetService } from "../Services/assetService";
 
+const { validationResult } = require('express-validator');
 var authMiddleware = require("../Middleware/AuthMiddleware");
+const assetService = new AssetService();
 
 export const assignmentRoutes = Router();
 const assignmentController = new AssignmentController;
@@ -46,8 +48,8 @@ assignmentRoutes.route("")
         [
          body("assignee").exists().withMessage("Assignee is required").isInt().withMessage('Assignee must be a valid user ID'),
          body("asset").exists().withMessage("Asset is required").isInt().withMessage('Asset must be a valid asset ID'),
-         body("startOfAssignment").exists().withMessage("Start of assignment is required").isISO8601().withMessage("Start of assignment must be a valid ISO 8601 date-time"),
-         body("endOfAssignment").optional().isISO8601().withMessage("End of assignment must be a valid ISO 8601 date-time")
+         // body("startOfAssignment").exists().withMessage("Start of assignment is required").isISO8601().withMessage("Start of assignment must be a valid ISO 8601 date-time"),
+         // body("endOfAssignment").optional().isISO8601().withMessage("End of assignment must be a valid ISO 8601 date-time")
         ],
         authMiddleware.isCustodian,
         handleValidationErrors,
@@ -59,11 +61,23 @@ assignmentRoutes.route("")
                 return res.status(400).json({ errors: errors.array() });
             }
             
+            // Check if the requested asset is the current user's custodianship
+            let asset = await assetService.getAssetById(req.body.asset as number)
+            if (asset && asset.custodian !== req.session.user?.id) {
+                return res.sendStatus(401);
+            }
+
+            // // Check if there is already an active assignment for the asset
+            // let res = await assignmentService.getActiveAssignmentFor(assetID)
+            
+            const start = new Date(Date.now())
             let data = {
                 assignee: req.body.assignee as number,
                 asset: req.body.asset as number,
-                startOfAssignment: req.body.startOfAssignment as Date, 
-                endOfAssignment: req.body.endOfAssignment ? req.body.endOfAssignment as Date : null,
+                // startOfAssignment: req.body.startOfAssignment as Date, 
+                // endOfAssignment: req.body.endOfAssignment ? req.body.endOfAssignment as Date : null,
+                startOfAssignment: start,
+                endOfAssignment: null,
             }
 
             return await assignmentController.createAssignment(data, res);
