d5c2d4c9640cf73756e607bb37f6486b4af58eb3
Jenny Zhang <jenny.zhang@canada.ca>
2024-09-09 07:25:00 -0400

    added additional data types asset and custodian

--- Frontend/src/hooks/AuthProvider.tsx
+++ Frontend/src/hooks/AuthProvider.tsx
@@ -1,55 +1,55 @@
 import { useContext, createContext, useState, PropsWithChildren } from 'react';
 import { loginUser, loadUser as load, logoutUser } from '../requests/auth';
 // import { useNavigate } from 'react-router-dom'
-import { user } from '../types/user';
+import { user } from '../types/data';
 
 type IAuthContextType = {
-    user: user | null;
-    logIn: (email: string, password: string) => Promise<void>;
-    logOut: () => Promise<void>;
-    loadUser: () => Promise<void>;
+  user: user | null;
+  logIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+  loadUser: () => Promise<void>;
 };
 
 const initialValue = {
-    user: null,
-    logIn: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
-    logOut: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
-    loadUser: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
+  user: null,
+  logIn: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
+  logOut: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
+  loadUser: () => new Promise<void>((resolve) => setTimeout(resolve, 1000)),
 };
 
 const AuthContext = createContext<IAuthContextType>(initialValue);
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
-    const [user, setUser] = useState<user | null>(initialValue.user);
-    // const navigate = useNavigate()
+  const [user, setUser] = useState<user | null>(initialValue.user);
+  // const navigate = useNavigate()
 
-    const logIn = async (email: string, password: string) => {
-        const res = await loginUser({ email, password });
-        const userData = res;
-        setUser(userData);
-        // navigate('/dashboard')
-    };
+  const logIn = async (email: string, password: string) => {
+    const res = await loginUser({ email, password });
+    const userData = res;
+    setUser(userData);
+    // navigate('/dashboard')
+  };
 
-    const logOut = async () => {
-        await logoutUser();
-        setUser(null);
-    };
+  const logOut = async () => {
+    await logoutUser();
+    setUser(null);
+  };
 
-    const loadUser = async () => {
-        const res = await load();
-        const userData = res;
-        setUser(userData);
-    };
+  const loadUser = async () => {
+    const res = await load();
+    const userData = res;
+    setUser(userData);
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, logIn, logOut, loadUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, logIn, logOut, loadUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
---
+++ Frontend/src/types/data.ts
@@ -0,0 +1,36 @@
+type user = {
+    UID: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    role: 'Admin' | 'Custodian' | 'Base';
+};
+
+type asset = {
+    id: number;
+    name: string;
+    type: string;
+    make?: string;
+    assetTag: string;
+    serialNumber?: string;
+    description?: string;
+    location?: string;
+    warrantyStartDate?: string;
+    warrantyEndDate?: string;
+    warrantyDetails?: string;
+    dateOfPurchase?: string;
+    cost?: number;
+    purchaser?: string;
+    comment?: string;
+    model?: string;
+    custodian: custodian;
+}
+
+type custodian = {
+    id: number;
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+}
+
+export type { user, asset, custodian }
