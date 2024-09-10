d75a76ff9fb4e199d52609341af4f2f5e151408d
Jenny Zhang <jenny.zhang@canada.ca>
2024-09-10 12:13:49 -0400

    clean up dashboard

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,136 +1,134 @@
 import MainTemplate from '../templates/MainTemplate';
 import {
-    GcdsContainer,
-    GcdsHeading,
-    GcdsLink,
-    GcdsText,
+  GcdsContainer,
+  GcdsDateModified,
+  GcdsHeading,
+  GcdsLink,
+  GcdsText,
 } from '@cdssnc/gcds-components-react';
 import {
-    TableContainer,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
 } from '@chakra-ui/react';
 import { useAuth } from '../hooks/AuthProvider';
 import { useEffect, useState } from 'react';
 import { fetchGet } from '../requests/requests';
 
 function Dashboard() {
-    const [userAssets, setUserAssets] = useState<asset[]>([]);
-    const { user } = useAuth();
+  const [userAssets, setUserAssets] = useState<asset[]>([]);
+  const { user } = useAuth();
 
-    const [error, setError] = useState<null | string>(null);
-    const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        // fetch data
-        const dataFetch = async () => {
-            try {
-                const assets = (await fetchUserAssets()) as asset[];
-                // set state when the data received
-                setUserAssets(assets);
-            } catch (err) {
-                setError((err as Error).message);
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      try {
+        const assets = (await fetchUserAssets()) as asset[];
+        // set state when the data received
+        setUserAssets(assets);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        dataFetch();
-    }, []);
+    dataFetch();
+  }, []);
 
-    return (
-        <MainTemplate>
-            <GcdsHeading tag="h1">Dashboard</GcdsHeading>
-            <GcdsText>
-                Welcome, {user?.firstName} {user?.lastName} ({user?.email}) !
-            </GcdsText>
-            <GcdsHeading tag="h2" marginBottom="0">
-                Your assets
-            </GcdsHeading>
-            <GcdsHeading tag="h3" marginTop="400">
-                Assigned to you
-            </GcdsHeading>
-            <GcdsText>
-                Here are all the assets that are currently assigned to you.
-            </GcdsText>
-            <GcdsContainer border padding="400" margin="0">
-                <GcdsText size="caption">
-                    Showing {userAssets.length} results.
-                </GcdsText>
-                <TableContainer>
-                    <Table>
-                        {/* <TableCaption>
+  return (
+    <MainTemplate>
+      <GcdsHeading tag="h1">Dashboard</GcdsHeading>
+      <GcdsText>
+        Welcome, {user?.firstName} {user?.lastName} ({user?.email}) !
+      </GcdsText>
+      <GcdsHeading tag="h2" marginBottom="0">
+        Your assets
+      </GcdsHeading>
+      <GcdsHeading tag="h3" marginTop="400">
+        Assigned to you
+      </GcdsHeading>
+      <GcdsText>
+        Here are all the assets that are currently assigned to you.
+      </GcdsText>
+      <GcdsContainer border padding="400" margin="0">
+        <GcdsText size="caption">Showing {userAssets.length} results.</GcdsText>
+        <TableContainer>
+          <Table>
+            {/* <TableCaption>
                             Table of your current assigned assets
                         </TableCaption> */}
-                        <Thead>
-                            <Tr>
-                                <Th>Asset</Th>
-                                <Th>Asset tag</Th>
-                                <Th>Type</Th>
-                                <Th>Assignment start date</Th>
-                            </Tr>
-                        </Thead>
-                        {loading ? (
-                            <Tbody>Loading....</Tbody>
-                        ) : error ? (
-                            <Tbody>Error: {error}</Tbody>
-                        ) : userAssets.length === 0 ? (
-                            <Tbody>No records</Tbody>
-                        ) : (
-                            <Tbody>
-                                {userAssets.map((asset) => {
-                                    return (
-                                        <Tr key={asset.id}>
-                                            <Td>
-                                                <GcdsLink
-                                                    href={`/assets/${asset.id}`}
-                                                >
-                                                    {asset.name}
-                                                </GcdsLink>
-                                            </Td>
-                                            <Td>{asset.assetTag}</Td>
-                                            <Td>{asset.type}</Td>
-                                            <Td>
-                                                {asset.assignedOn.toDateString()}
-                                            </Td>
-                                        </Tr>
-                                    );
-                                })}
-                            </Tbody>
-                        )}
-                    </Table>
-                </TableContainer>
-            </GcdsContainer>
-        </MainTemplate>
-    );
+            <Thead>
+              <Tr>
+                <Th>Asset</Th>
+                <Th>Asset tag</Th>
+                <Th>Type</Th>
+                <Th>Assignment start date</Th>
+              </Tr>
+            </Thead>
+            {loading ? (
+              <Tbody>Loading....</Tbody>
+            ) : error ? (
+              <Tbody>Error: {error}</Tbody>
+            ) : userAssets.length === 0 ? (
+              <Tbody>No records</Tbody>
+            ) : (
+              <Tbody>
+                {userAssets.map((asset) => {
+                  return (
+                    <Tr key={asset.id}>
+                      <Td>
+                        <GcdsLink href={`/asset/${asset.id}`}>
+                          {asset.name}
+                        </GcdsLink>
+                      </Td>
+                      <Td>{asset.assetTag}</Td>
+                      <Td>{asset.type}</Td>
+                      <Td>{asset.assignedOn.toDateString()}</Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            )}
+          </Table>
+        </TableContainer>
+      </GcdsContainer>
+      <GcdsContainer size="xl" centered style={{ paddingBottom: 10 }}>
+        <GcdsDateModified> 2024-09-10 </GcdsDateModified>
+      </GcdsContainer>
+    </MainTemplate>
+  );
 }
 
 export default Dashboard;
 
 type asset = {
-    id: number;
-    name: string;
-    assetTag: string;
-    type: string;
-    assignedOn: Date;
+  id: number;
+  name: string;
+  assetTag: string;
+  type: string;
+  assignedOn: Date;
 };
 
 async function fetchUserAssets() {
-    const response = await fetchGet('/api/asset/user');
-    if (response.ok) {
-        const assets = await response.json();
-        return assets.map((a: asset) => {
-            return { ...a, assignedOn: new Date(a.assignedOn) };
-        });
-    } else {
-        throw new Error(
-            'fetchUserAssets Error:' + response.status + (await response.text())
-        );
-    }
+  const response = await fetchGet('/api/asset/user');
+  if (response.ok) {
+    const assets = await response.json();
+    return assets.map((a: asset) => {
+      return { ...a, assignedOn: new Date(a.assignedOn) };
+    });
+  } else {
+    throw new Error(
+      'fetchUserAssets Error:' + response.status + (await response.text())
+    );
+  }
 }
 // const exampleAssets = [
 //     {
