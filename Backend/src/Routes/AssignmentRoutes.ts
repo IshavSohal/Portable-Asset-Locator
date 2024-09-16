daa97feddbc2375fdc672c99b28ef73881d3fd3d
Gordon Lin <Gordon.Lin@ec.gc.ca>
2024-09-16 11:41:00 -0400

    Finish assign/unassign user functionality, UI changes

--- Backend/src/Routes/AssignmentRoutes.ts
+++ Backend/src/Routes/AssignmentRoutes.ts
@@ -4,103 +4,128 @@ import { ConsoleLogger } from "../Logging/ConsoleLogger";
 import { body } from "express-validator";
 import { AssetService } from "../Services/assetService";
 
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 var authMiddleware = require("../Middleware/AuthMiddleware");
 const assetService = new AssetService();
 
 export const assignmentRoutes = Router();
-const assignmentController = new AssignmentController;
+const assignmentController = new AssignmentController();
 
-const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    ConsoleLogger.logWarning(errors.errors);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-}
+const handleValidationErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  ConsoleLogger.logWarning(errors.errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 /**
  * End an assignment - this sets the end date of the assignment
  * to the current time if processed successfully
  */
 assignmentRoutes.get(
-    '/end/:id',
-    authMiddleware.isCustodian,
-    async (req: Request, res: Response) => {
-        ConsoleLogger.logInfo('Requesting unassignment attempt');
-        const errors = validationResult(req);
+  "/end/:id",
+  authMiddleware.isCustodian,
+  async (req: Request, res: Response) => {
+    ConsoleLogger.logInfo("Requesting unassignment attempt");
+    const errors = validationResult(req);
 
-        // If JSON validation fails, send a 400, Conflict
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        let id = parseInt(req.params.id, 10)
-
-        return await assignmentController.endAssignment(id, res)
+    // If JSON validation fails, send a 400, Conflict
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-)
+
+    let id = parseInt(req.params.id, 10);
+
+    return await assignmentController.endAssignment(id, res);
+  }
+);
 
 /**
  *  Get the assignment given the assignment ID
  */
-assignmentRoutes.get(
-    '/:id', 
-    async (req: Request, res: Response) => {
-        ConsoleLogger.logInfo('Requesting assignment attempt');
-        const errors = validationResult(req);
+assignmentRoutes.get("/:id", async (req: Request, res: Response) => {
+  ConsoleLogger.logInfo("Requesting assignment attempt");
+  const errors = validationResult(req);
 
-        // If JSON validation fails, send a 400, Conflict
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+  // If JSON validation fails, send a 400, Conflict
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-        let id = parseInt(req.params.id, 10)
+  let id = parseInt(req.params.id, 10);
 
-        return await assignmentController.getAssignment(id, res)
-    }
-)
+  return await assignmentController.getAssignment(id, res);
+});
+
+/**
+ *  Get the current assignment given the asset ID
+ */
+assignmentRoutes.get("/asset/:id", async (req: Request, res: Response) => {
+  ConsoleLogger.logInfo("Requesting current asset assignment attempt");
+  const errors = validationResult(req);
+
+  // If JSON validation fails, send a 400, Conflict
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  let id = parseInt(req.params.id, 10);
+
+  return await assignmentController.getActiveAssetAssignment(id, res);
+});
 
 /**
  * Create an assignment
  */
-assignmentRoutes.route("")
-    .post(
-        [
-         body("assignee").exists().withMessage("Assignee is required").isInt().withMessage('Assignee must be a valid user ID'),
-         body("asset").exists().withMessage("Asset is required").isInt().withMessage('Asset must be a valid asset ID'),
-         // body("startOfAssignment").exists().withMessage("Start of assignment is required").isISO8601().withMessage("Start of assignment must be a valid ISO 8601 date-time"),
-         // body("endOfAssignment").optional().isISO8601().withMessage("End of assignment must be a valid ISO 8601 date-time")
-        ],
-        authMiddleware.isCustodian,
-        handleValidationErrors,
-        async (req: Request, res: Response) => {
-            ConsoleLogger.logInfo('Creating an assignment');
-            const errors = validationResult(req);
-            // If JSON validation fails, send a 400, Conflict
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            }
-            
-            // Check if the requested asset is the current user's custodianship
-            let asset = await assetService.getAssetById(req.body.asset as number)
-            if (asset && asset.custodian !== req.session.user?.id) {
-                return res.sendStatus(401);
-            }
-            
-            const start = new Date(Date.now())
-            let data = {
-                assignee: req.body.assignee as number,
-                asset: req.body.asset as number,
-                // startOfAssignment: req.body.startOfAssignment as Date, 
-                // endOfAssignment: req.body.endOfAssignment ? req.body.endOfAssignment as Date : null,
-                startOfAssignment: start,
-                endOfAssignment: null,
-            }
+assignmentRoutes.route("").post(
+  [
+    body("assignee")
+      .exists()
+      .withMessage("Assignee is required")
+      .isInt()
+      .withMessage("Assignee must be a valid user ID"),
+    body("asset")
+      .exists()
+      .withMessage("Asset is required")
+      .isInt()
+      .withMessage("Asset must be a valid asset ID"),
+    // body("startOfAssignment").exists().withMessage("Start of assignment is required").isISO8601().withMessage("Start of assignment must be a valid ISO 8601 date-time"),
+    // body("endOfAssignment").optional().isISO8601().withMessage("End of assignment must be a valid ISO 8601 date-time")
+  ],
+  authMiddleware.isCustodian,
+  handleValidationErrors,
+  async (req: Request, res: Response) => {
+    ConsoleLogger.logInfo("Creating an assignment");
+    const errors = validationResult(req);
+    // If JSON validation fails, send a 400, Conflict
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-            return await assignmentController.createAssignment(data, res);
-        }
-    );
+    // Check if the requested asset is the current user's custodianship
+    let asset = await assetService.getAssetById(req.body.asset as number);
+    if (asset && asset.custodian !== req.session.user?.id) {
+      return res.sendStatus(401);
+    }
+
+    const start = new Date(Date.now());
+    let data = {
+      assignee: req.body.assignee as number,
+      asset: req.body.asset as number,
+      // startOfAssignment: req.body.startOfAssignment as Date,
+      // endOfAssignment: req.body.endOfAssignment ? req.body.endOfAssignment as Date : null,
+      startOfAssignment: start,
+      endOfAssignment: null,
+    };
+
+    return await assignmentController.createAssignment(data, res);
+  }
+);
 
 module.exports = assignmentRoutes;
--- Backend/src/Routes/UserRoutes.ts
+++ Backend/src/Routes/UserRoutes.ts
@@ -1,17 +1,37 @@
 import { Request, Response, Router } from "express";
 import { UserController } from "../Controllers/UserController";
+import { ConsoleLogger } from "../Logging/ConsoleLogger";
+import { validationResult } from "express-validator";
 var authMiddleware = require("../Middleware/AuthMiddleware");
 
 const userRoutes = Router();
-const userController = new UserController;
+const userController = new UserController();
 
 userRoutes.get(
-    "/", 
-    authMiddleware.isAuthenticated,
-    async (req: Request, res: Response) => {
-        return await userController.getUsers(res);
+  "/",
+  authMiddleware.isAuthenticated,
+  async (req: Request, res: Response) => {
+    return await userController.getUsers(res);
+  }
+);
+
+// Get user data from ID
+userRoutes.get(
+  "/:userID",
+  authMiddleware.isAuthenticated,
+  async (req: Request, res: Response) => {
+    ConsoleLogger.logInfo(`Get user info attempt: ${req.params.userID}`);
+    const errors = validationResult(req);
+
+    // If JSON validation fails, send a 400, Conflict
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const userID = parseInt(req.params.userID);
+
+    return await userController.getUserById(userID, res);
+  }
 );
 
 module.exports = userRoutes;
-
